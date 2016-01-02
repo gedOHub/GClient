5de044d0de9cff7673f167b619ccee97e520e86a
Gediminas Jančys <g.jancys@gmail.com>
2016-01-02 04:00:46 +0200

    Sukurtas testas tikrinantis ar ServerSocket objektas klausosi ir laukia prisjungimu

--- Testing/ServerSocketTest/UnitTest.cpp
+++ Testing/ServerSocketTest/UnitTest.cpp
@@ -1,11 +1,15 @@
 #include "stdafx.h"
 
 #include "GClientLib.h"
+#include "Globals.h"
+#include <winsock2.h>
 
 using namespace System;
 using namespace System::Text;
 using namespace System::Collections::Generic;
 using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace System::Diagnostics;
+using namespace GClientLib;
 
 namespace ServerSocketTest
 {
@@ -55,11 +59,23 @@ namespace ServerSocketTest
 		#pragma endregion 
 
 		[TestMethod]
-		void TestMethod1()
+		void IsServerSocketListenning()
 		{
-			//
-			// TODO: Add test logic here
-			//
+			// --- Select funkcijos kintamieji ---
+			// Inicijuoju
+			fd_set skaitomiSocket, rasomiSocket, klaidingiSocket;
+			// Nunulinu strukturas
+			FD_ZERO(&skaitomiSocket);
+			FD_ZERO(&rasomiSocket);
+			FD_ZERO(&klaidingiSocket);
+			// Sukuriu ServerSocket tipo kintamaji
+			ServerSocket^ server = gcnew ServerSocket("localhost", 0, &skaitomiSocket, &rasomiSocket, &klaidingiSocket);
+			// Tarpiniai kintamieji
+			int error_code;
+			int error_code_size = sizeof(error_code);
+			// Tikrinu ar prievadu yra klausomasi
+			// Placiau https://msdn.microsoft.com/en-us/library/windows/desktop/ms738544(v=vs.85).aspx
+			Assert::AreEqual(0, getsockopt(server->GetSocket(), SOL_SOCKET, SO_ACCEPTCONN, (char*)&error_code, &error_code_size));
 		};
 	};
 }
